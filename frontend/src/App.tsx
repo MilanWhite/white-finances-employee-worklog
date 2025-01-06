b4e10b35db5224796f0c7c7e8e63478a9e59a0a3
Milan White <milan.white.25@gmail.com>
2025-01-06 11:19:07 -0500

    refactor: remove unnecessary useEffect calls

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,6 @@
 import { HashRouter, Routes, Route } from "react-router-dom";
 
 import { useAuth } from "../context/AuthContext";
-import { useCallback, useEffect, useState } from "react";
 import { ManagerRoute, ProtectedRoute, PublicRoute } from "./routes/Routes";
 
 import { EmployeeLogin } from "../pages/EmployeeLogin";
@@ -10,11 +9,6 @@ import { ManagerLogin } from "../pages/ManagerLogin";
 import { ManagerDashboard } from "../pages/ManagerDashboard";
 import { EmployeeDashboard } from "../pages/EmployeeDashboard";
 
-import apiClient from "../services/api-client";
-import axios from "axios";
-
-import { Helmet } from "react-helmet";
-import { getCsrfToken } from "../services/csrfTokenManager";
 
 interface Props {
     isLoggedIn: boolean;
@@ -25,7 +19,7 @@ const DefaultRoute = ({ isLoggedIn, isManager }: Props) => {
     const { isAuthInitializing } = useAuth();
 
     if (isAuthInitializing) {
-        return;
+        return ;
     }
 
     if (isLoggedIn) {
@@ -36,38 +30,11 @@ const DefaultRoute = ({ isLoggedIn, isManager }: Props) => {
 };
 
 function App() {
-    useEffect(() => {
-        const prefetchToken = async () => {
-            await getCsrfToken(); // Fetch and cache the token
-        };
-
-        prefetchToken();
-    }, []);
-
-    const [csrfToken, setCsrfToken] = useState("");
-
-    useEffect(() => {
-        async function fetchCsrfToken() {
-            try {
-                const response = await apiClient.get<{ csrf_token: string }>(
-                    "/api/csrf-token"
-                );
-                setCsrfToken(response.data.csrf_token);
-            } catch (error) {
-                console.error("Failed to fetch CSRF token:", error);
-            }
-        }
-
-        fetchCsrfToken();
-    }, []);
 
     const { isLoggedIn, isManager } = useAuth();
 
     return (
         <>
-            <Helmet>
-                {csrfToken && <meta name="csrf-token" content={csrfToken} />}
-            </Helmet>
             <HashRouter>
                 <Routes>
                     <Route
